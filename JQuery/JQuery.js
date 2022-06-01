ae34a8270f671f798cec3def03664daeddbd73ff
Fireball11 <oparin.ilya@inbox.ru>
2022-06-01 22:09:16 +0300

    Конец 14 урока

--- JQuery/JQuery.js
+++ JQuery/JQuery.js
@@ -6,7 +6,7 @@ function Hide_L1() {
 }
 
 // Обрезка пробелов
-function CutSpaces () {
+function CutSpaces() {
     var str = "           s a sssi     "
     alert(str)
     alert($.trim(str)) // trim обрезает лишние пробелы
@@ -33,13 +33,44 @@ function chkValid() {
 // $("img").not("img[alt]") - Тоже что и сверху
 // .attr( 'Атрибут' , 'Значение' ) - Присвоить атрибут
 
-function Alt () {
-    $(":not(img[alt])").attr ('alt',"JQuery")
+function Alt() {
+    $(":not(img[alt])").attr('alt', "JQuery")
     var elements = $("img");
     var text = '';
     for (let i = 0; i < elements.length; i++) {
         text += elements[i].alt + "\n";
-        // вместо .alt можно использовать любой атр, кроме class. В случае с классом - className
+        // Вместо .alt можно использовать любой атр, кроме class. 
+        // В случае с классом - className
     }
     alert(text)
+}
+
+function divBGC() {
+    // console.log('text')
+    var div = prompt("1 or 2");
+    while (div != 1 && div != 2) {
+        alert("write 1 or 2")
+        div = prompt("1 or 2")
+    }
+    $("#div").attr("class", 'div_' + div);
+    alert($('#div').className())
+}
+
+function Style () { // 44 строчка
+    $("#div2").css("font-size", "25px")
+    $("#div2").css("color", "red")
+    $("#div2").css("width", "250px")
+    $("#div2").css("background-color", function () {
+        return 'blue';
+    }) 
+    $("#div2").css("height", function () {
+        return $(this).width();
+    })
+}
+
+function HtmlnText () {
+    alert($('#div3').html());
+    alert($('#div3').text());
+    
+    $('#div3').html("<p>Новый абзац</p>")
 }
