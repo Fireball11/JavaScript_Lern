07c07c3f2b4ddaa5decd66138f25a27902940350
Fireball11 <oparin.ilya@inbox.ru>
2022-06-09 10:33:32 +0300

    Expression

---
+++ JavaScript/function/Expression/Expression.js
@@ -0,0 +1,17 @@
+function showText(text) {
+    alert(text)
+} // Function Declaration 
+
+showText()
+
+var showT = function(text) {
+    alert(text)
+} // Function Expression
+
+showT()
+
+// Дикларейшон можно вызвать везде внутри 
+// блока где объявлена функция.
+
+// Експрешон можно вызвать только после 
+// объявления в любой части документа.
